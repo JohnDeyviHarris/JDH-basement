17fb09d401a70b0ef0b655453be201b924c55e91
Lukproposhalko <166041627+Lukproposhalko@users.noreply.github.com>
2024-06-13 18:46:54 +0300

    Summary

--- Assets/Scripts/Game/PurpleLabirintEnd.cs
+++ Assets/Scripts/Game/PurpleLabirintEnd.cs
@@ -9,6 +9,7 @@ public class PurpleLabirintEnd : MonoBehaviour
     [SerializeField] private GameObject wall;
     [SerializeField] private AudioSource mainsong;
     [SerializeField] private AudioSource c4;
+    [SerializeField] private Animator KaboomAnimator;
     private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") 
@@ -18,12 +19,14 @@ public class PurpleLabirintEnd : MonoBehaviour
             secondCam.SetActive(true);
             mainCam.SetActive(false);  
             yield return new WaitForSeconds(11);
+            KaboomAnimator.SetTrigger("KaBoom");
+            yield return new WaitForSeconds(0.25f);
             wall.SetActive(false);
             yield return new WaitForSeconds(2);
             mainsong.Play();
             mainCam.SetActive(true);
             secondCam.SetActive(false);
-
+            Destroy(this);
         }
     }
 }
