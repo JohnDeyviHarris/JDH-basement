6a373a0265ee694a793a49ac8b613e4355409b2a
Lukproposhalko <166041627+Lukproposhalko@users.noreply.github.com>
2024-06-20 18:35:58 +0300

    Summary

---
+++ Assets/Scripts/Game/TumbochkaManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbochkaManager : MonoBehaviour
+{
+    private int tumbochkaCount;
+    [SerializeField] private GameObject walls;
+}
--- Assets/Scripts/Lololowka/Movement.cs
+++ Assets/Scripts/Lololowka/Movement.cs
@@ -10,6 +10,7 @@ public class Movement : MonoBehaviour
     public float speed;
     private bool start = false;
     private bool isLay = false;
+    [SerializeField] private bool layonstart;
     [SerializeField] private AudioSource MainSong;
 
     public event Action Moved;
@@ -18,16 +19,18 @@ public class Movement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        LieDown();
+        if (layonstart)
+        {
+            LieDown();
+        }
     }
     private void FixedUpdate()
     {
         Vector2 velocity = Vector2.zero;
-        velocity.y = Input.GetAxis("Vertical") * speed;
-        velocity.x = Input.GetAxis("Horizontal") * speed;
+        velocity.y = Input.GetAxisRaw("Vertical") * speed;
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
         if (!isLay) rb.velocity = velocity;
-        if (rb.velocity != Vector2.zero) Moved.Invoke();
-        if (isLay && velocity.x != 0 || isLay && velocity.y != 0) rb.velocity = velocity;
+        if (velocity != Vector2.zero) Moved?.Invoke();
         if (Input.GetKey(KeyCode.Z))
         {
             LieDown();
@@ -36,6 +39,7 @@ public class Movement : MonoBehaviour
     public void LieDown()
     {
         if (isLay) return;
+        rb.velocity = Vector2.zero;
         transform.rotation = Quaternion.Euler(0, 0, 90);
         MainSong.Stop();
         Moved += StandUp;
